c00d2cc6c8c52c8bafa27f4a49cba10ec7477e08
Lukasz Markowski <lucas20193@gmail.com>
2021-04-29 15:52:12 +0200

    add Header comp and routing

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,42 @@
+import { Button } from '@material-ui/core'
+import React from 'react'
+import './Header.scss'
+
+import SearchIcon from '@material-ui/icons/Search';
+import { IconButton } from '@material-ui/core';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink
+} from "react-router-dom";
+
+const Header = () => {
+  return ( 
+    <div className="header">
+      <div className="header__logoWrapper">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/BTC_Logo.svg/2000px-BTC_Logo.svg.png" alt="" className="header__logo"/>
+      </div>
+      <ul className="header__nav">
+        <li><NavLink exact to="/">Cryptos</NavLink></li>
+        <li><NavLink to="/portfolio">Portfolio</NavLink></li>
+        
+      </ul>
+      <div className="header__searchBar">
+      <IconButton size="small">
+        <SearchIcon />
+      </IconButton>
+      <input
+        type="text"
+        placeholder="Search"/>
+      </div>
+      <div className="header__login">
+        <Button 
+        variant="contained" color="primary"
+        disableRipple={true}>Log in</Button>
+      </div>
+    </div>
+    );
+}
+
+export default Header;
